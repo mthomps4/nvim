1ca68c3991d03229bbf3a0d44fd8bd1dc5754f9b
Matt Thompson <matt@echobind.com>
2024-12-17 10:38:36 -0500

    extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,13 @@ return {
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader><leader>", builtin.find_files, {desc = "Find Files"})
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
