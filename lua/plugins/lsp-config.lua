09965c2adb14ea39ef316a53b847917e07cbe899
Matt Thompson <matt@echobind.com>
2025-07-16 15:15:44 -0400

    updates to nvim .11 and plugs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,22 +20,31 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.tailwindcss.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.ruby_lsp.setup({
         capabilities = capabilities,
         cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        init_options = {
+          formatter = "rubocop",
+          linters = { "rubocop" },
+          rubocop = {
+            command = "bundle",
+            args = { "exec", "rubocop" },
+          },
+        },
         settings = {
           rubyLsp = {
-            formatter = 'auto',
-          }
+            formatter = "auto",
+          },
         },
       })
+
       lspconfig.html.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
 
       -- Add autoformat on save
@@ -45,12 +54,11 @@ return {
       --   end,
       -- })
 
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
       vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format File" })
     end,
   },
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,19 @@
 return {
   "rcarriga/nvim-notify",
+  version = "v3.14",
+  priority = 1000,
+  lazy = false,
   config = function()
-    vim.notify = require("notify")
+    local notify = require("notify")
+    notify.setup({
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    })
+    vim.notify = notify
   end,
 }
